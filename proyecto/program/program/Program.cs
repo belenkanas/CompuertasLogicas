51807a9764d7f7db4ce00f8302b87f246c044493
Belén Kanas <maria.kanas@correo.ucu.edu.uy>
2024-09-16 23:37:11 -0300

    Program

--- proyecto/program/program/Program.cs
+++ proyecto/program/program/Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using program;
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Compuerta And con sus entradas
+        CompuertaAND and1 = new CompuertaAND("AND-1", LogicalTrue.Instance);
+        and1.AddInput(LogicalTrue.Instance);
+        Console.WriteLine($"Resultado intermedio de compuerta {and1.Name} : {and1.Output}");
+        
+        //Compuerta Or con sus entradas
+        CompuertaOR or2 = new CompuertaOR("OR-2", LogicalFalse.Instance);
+        or2.AddInput(and1);
+        Console.WriteLine($"Resultado intermedio de compuertas {or2.Name}: {or2.Output}");
+        
+        //Compuerta not
+        CompuertaNOT not3 = new CompuertaNOT("NOT-3", or2);
+        Console.WriteLine($"Resultado intermedio de compuertas {not3.Name}: {not3.Output}");
+    }
+}
+
