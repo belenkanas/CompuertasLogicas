810e69eb537f45ec0dcb8398ed2efd2846528ab7
Belén Kanas <maria.kanas@correo.ucu.edu.uy>
2024-09-18 11:41:22 -0300

    GarageGate

--- proyecto/program/program/Program.cs
+++ proyecto/program/program/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+       //Ejemplo de prueba:
         //Compuerta And con sus entradas
         CompuertaAND and1 = new CompuertaAND("AND-1", LogicalTrue.Instance);
         and1.AddInput(LogicalTrue.Instance);
@@ -16,7 +17,34 @@ class Program
         
         //Compuerta not
         CompuertaNOT not3 = new CompuertaNOT("NOT-3", or2);
-        Console.WriteLine($"Resultado intermedio de compuertas {not3.Name}: {not3.Output}");
+        Console.WriteLine($"Resultado intermedio de compuertas {not3.Name}: {not3.Output} \n");
+        
+        
+        
+        //Ejemplo de GarageGate:
+
+        //Primer compuerta (ab)
+        CompuertaAND ab = new CompuertaAND("AB", LogicalTrue.Instance); //primero comienza con la entrada a
+        ab.AddInput(LogicalTrue.Instance); // le agrega la entrada b.
+        Console.WriteLine($"{ab.Output}");
+
+        
+        //Segunda compuerta (/ab)
+        CompuertaNOT aNegado = new CompuertaNOT("/A", LogicalTrue.Instance);
+        CompuertaNOT bNegado = new CompuertaNOT("/B", LogicalTrue.Instance);
+        CompuertaAND abNegado = new CompuertaAND("/AB", aNegado);
+        abNegado.AddInput(bNegado);
+        Console.WriteLine($"{abNegado.Output}");
+        
+        //Suma de compuertas (ab + /ab)
+        CompuertaOR abORab = new CompuertaOR("AB+/AB", ab);
+        abORab.AddInput(abNegado);
+        Console.WriteLine(abORab.Output);
+        
+        //Ultima compuerta (ab + /ab)c
+        CompuertaAND abORc = new CompuertaAND("ABC", LogicalTrue.Instance);
+        abORc.AddInput(abORab);
+        Console.WriteLine(abORc.Output);
     }
 }
 
