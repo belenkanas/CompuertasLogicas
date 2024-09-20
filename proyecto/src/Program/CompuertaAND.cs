8d13bdff92598dd2ec1f05b20962143b53f3ad31
Belén Kanas <maria.kanas@correo.ucu.edu.uy>
2024-09-20 13:10:29 -0300

    Comentarios clases

--- proyecto/src/Program/CompuertaAND.cs
+++ proyecto/src/Program/CompuertaAND.cs
@@ -2,12 +2,15 @@
 
 namespace program;
 
+/*
+ * Clase que implementa los cálculos necesarios según el Algebra de Boole para la operación AND cuando se tienen entradas de caracter true o false.
+ */
 public class CompuertaAND : IValorVerdad
 {
     public string Name { get; set; }
-    private List<IValorVerdad> inputs = new List<IValorVerdad>();
+    private List<IValorVerdad> inputs = new List<IValorVerdad>(); //Para poder calcular el resultado, se crea una lista con todas las entradas que recibe la clase en cada parte del programa.
 
-    public CompuertaAND(string name, IValorVerdad firstInput)
+    public CompuertaAND(string name, IValorVerdad firstInput) //Al crear el objeto, se pide el nombre del mismo y una primer entrada, la cual puede ser de valor verdadero o falso (LogicalTrue o LogicalFalse)
     {
         this.Name = name;
         this.inputs.Add(firstInput);
@@ -18,7 +21,7 @@ public class CompuertaAND : IValorVerdad
         get
         {
             bool result = this.inputs[0].Output;
-            for (int i = 1; i < this.inputs.Count; i++)
+            for (int i = 1; i < this.inputs.Count; i++) // Si tiene más de una entrada, va a realizar una interación en cada elemento de la lista en la que implementa el uso de && para retornar un resultado booleano.
             {
                 result = result && this.inputs[i].Output;
             }
@@ -27,7 +30,7 @@ public class CompuertaAND : IValorVerdad
         }
     }
 
-    public void AddInput(IValorVerdad input)
+    public void AddInput(IValorVerdad input) //Como lo dice su nombre, mediante este método, se le agrega otra entrada a la lista para dsp calcular su resultado.
     {
         this.inputs.Add(input);
     }
--- proyecto/src/Program/IValorVerdad.cs
+++ proyecto/src/Program/IValorVerdad.cs
@@ -1,5 +1,9 @@
 ﻿namespace program
 {
+    /*
+     * Tipo IValorVerdad de la cual heredan las clases CompuertaAND, CompuertaNOT, CompuertaOR, LogicalTrue y LogicalFalse.
+     * Estas clases comparten un método en común, que es realizar un resultado (true o false) segun las entradas que recibe cada función booleana.
+     */
     public interface IValorVerdad
     {
         public bool Output { get; }
