49b395967c2bd3bd09f300edc6a7771c641b79c8
Belén Kanas <maria.kanas@correo.ucu.edu.uy>
2024-09-20 11:05:34 -0300

    Comentarios

--- proyecto/program/program/Program.cs
+++ proyecto/program/program/Program.cs
@@ -22,29 +22,28 @@ class Program
         
         
         //Ejemplo de GarageGate:
-
+        Console.WriteLine($"GarageGate:");
         //Primer compuerta (ab)
         CompuertaAND ab = new CompuertaAND("AB", LogicalTrue.Instance); //primero comienza con la entrada a
         ab.AddInput(LogicalTrue.Instance); // le agrega la entrada b.
-        Console.WriteLine($"{ab.Output}");
-
+        Console.WriteLine($"Resultado primer compuerta (ab): {ab.Output}");
         
         //Segunda compuerta (/ab)
         CompuertaNOT aNegado = new CompuertaNOT("/A", LogicalTrue.Instance);
         CompuertaNOT bNegado = new CompuertaNOT("/B", LogicalTrue.Instance);
         CompuertaAND abNegado = new CompuertaAND("/AB", aNegado);
         abNegado.AddInput(bNegado);
-        Console.WriteLine($"{abNegado.Output}");
+        Console.WriteLine($"Resultado de segunda compuerta (/ab): {abNegado.Output}");
         
         //Suma de compuertas (ab + /ab)
         CompuertaOR abORab = new CompuertaOR("AB+/AB", ab);
         abORab.AddInput(abNegado);
-        Console.WriteLine(abORab.Output);
+        Console.WriteLine($"Suma de compuertas: {abORab.Output}");
         
         //Ultima compuerta (ab + /ab)c
         CompuertaAND abORc = new CompuertaAND("ABC", LogicalTrue.Instance);
         abORc.AddInput(abORab);
-        Console.WriteLine(abORc.Output);
+        Console.WriteLine($"Resultado total de la función de las compuertas : (ab + /ab)c: {abORc.Output}");
     }
 }
 
