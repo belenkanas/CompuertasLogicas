79146a517063b21990ec018032eda5b7df57c7a8
Belén Kanas <maria.kanas@correo.ucu.edu.uy>
2024-09-20 13:17:14 -0300

    Comentarios IValorVerdad, LogicalTrue y LogicalFalse

--- proyecto/src/Program/LogicalFalse.cs
+++ proyecto/src/Program/LogicalFalse.cs
@@ -1,16 +1,15 @@
 ﻿namespace program;
 
-public class LogicalFalse: IValorVerdad
+public class LogicalFalse: IValorVerdad //Clase que hereda características del tipo IValorVerdad
 {
+    public static IValorVerdad Instance { get; } = new LogicalFalse(); //Esto es para que las otras clases, e incluso el programa, puedan realizar una instancia para sus operaciones con este valor.
     public LogicalFalse()
     {
-        
+        //Al instanciarla no se necesita ningún parámetro
     }
-
-    public static IValorVerdad Instance { get; } = new LogicalFalse();
-
+    
     public bool Output
     {
-        get { return false; }
+        get { return false; } //Su valor de retorno siempre será false.
     }
 }
--- proyecto/src/Program/LogicalTrue.cs
+++ proyecto/src/Program/LogicalTrue.cs
@@ -1,9 +1,13 @@
 ﻿namespace program;
 
-public class LogicalTrue: IValorVerdad
+public class LogicalTrue: IValorVerdad //Clase que hereda características de IValorVerdad, la cual es el método de retornar un valor dsp de cierta operación.
 {
-    public static IValorVerdad Instance { get; } = new LogicalTrue();
-    public bool Output
+    public static IValorVerdad Instance { get; } = new LogicalTrue(); 
+    public LogicalTrue()
+    {
+        
+    }
+    public bool Output //Siempre su retorno será el valor booleano de true, esta clase es una representación de esto mismo.
     {
         get
         {
